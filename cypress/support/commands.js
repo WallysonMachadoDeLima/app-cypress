e0837a160151c968911af626eb27b11181dad3ad
Wallyson <machadodelimawallyson@gmail.com>
2024-05-09 19:24:04 -0400

    chore: Remove unused files and update dependencies

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,29 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import './sections/secretaria/commands'
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.log('LIMPANDO: Dados de login')
+    cy.clearAllCookies()
+    cy.clearAllLocalStorage()
+    cy.clearAllSessionStorage()
+    cy.wait(500) 
+
+    cy.log('ACESSANDO: Portal do Cidadão')
+    cy.visit('')
+    cy.wait(500) 
+    cy.get('#wt-cli-accept-all-btn').click();
+    cy.wait(500)
+    cy.get('#dontShowAgainCheckbox').dblclick();
+    cy.wait(500)
+    cy.get('body > div.content > div.login-section > div > div.call-to-login').should('contain', 'Faça seu login na plataforma')
+
+    cy.log('PREENCHENDO: Dados de login')
+    cy.wait(500)
+    cy.get('#Username').clear().type(username)
+    cy.get('#Password').clear().type(password)
+    cy.wait(500)
+    cy.get('body > div.content > div.login-section > div > div.login-card > div.login-card-content > div.login-form > form > button').click()
+    cy.wait(500)
+    cy.get('body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span').should('contain', 'Bem-vindo(a)!')
+
+})
+
